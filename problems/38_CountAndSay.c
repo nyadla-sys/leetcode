c69a87e3693827528384bde03b14c90b5ee46b4a
Niranjan Yadla <yadlaniranjan@gmail.com>
2025-03-20 22:20:07 -0700

    Update on 03/20/2025

---
+++ problems/38_CountAndSay.c
@@ -0,0 +1,63 @@
+/*
+https://leetcode.com/problems/count-and-say/description/
+difficulty: medium
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+Given a positive integer n, return the nth element of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 4
+
+Output: "1211"
+
+Explanation:
+
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+Example 2:
+
+Input: n = 1
+
+Output: "1"
+
+Explanation:
+
+This is the base case.
+*/
+
+//Solution
+char* countAndSay(int n) {
+  char* next = (char*)malloc(5000);
+  char* current = (char*)malloc(5000);
+
+  strcpy(current, "1");
+
+  for (int i = 1; i < n; i++) {
+      int len = strlen(current);
+      int j = 0;
+      int k = 0;
+      while (j < len) {
+        char digit = current[j];
+        int count = 1;
+        while (j + 1 < len && current[j + 1] == digit) {
+            count++;
+            j++;
+        }
+        k += sprintf(next + k, "%d%c", count, digit);
+        j++;
+      }
+      strcpy(current, next);
+  }
+  free(next);
+  return current;
+}
